0d00059875b6420f36490684a22f01f893b00e93
christianotieno <christian.otieno@saltpay.co>
2023-07-03 01:19:30 +0100

    feat: add a config for db connection logic

---
+++ server/src/config/config.go
@@ -0,0 +1,14 @@
+package config
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func DbConnect() (db *sql.DB, err error) {
+	db, err = sql.Open("mysql", "root:password@tcp(localhost:3306)/task_manager")
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
