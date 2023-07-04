e730ee674d594808ae7f63b7e4bf099df4a7ba17
christianotieno <christian.otieno@saltpay.co>
2023-07-04 01:19:04 +0100

    feat: update task handler to factor in new operations

--- server/src/handlers/task_handler.go
+++ server/src/handlers/task_handler.go
@@ -2,43 +2,42 @@ package handlers
 
 import (
 	"github.com/christianotieno/tasks-traker-app/server/src/models"
-	"log"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 // CreateTaskHandler defines the route handler function for creating a task
 func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
-	// Connect to the database
-	db, err := openDbConnection()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
 	taskHandler := models.TaskHandler(db)
 	taskHandler.CreateTask(w, r)
-
-	err = closeDbConnection(db)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
 }
 
 // GetAllTasksHandler defines the route handler function for retrieving all tasks
 func GetAllTasksHandler(w http.ResponseWriter, r *http.Request) {
-	db, err := openDbConnection()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
 	taskHandler := models.TaskHandler(db)
 	taskHandler.GetAllTasks(w, r)
+}
 
-	err = closeDbConnection(db)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+// GetTaskHandler defines the route handler function for retrieving a task
+func GetTaskHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	taskHandler := models.TaskHandler(db)
+	taskHandler.GetTask(w, r, id)
+}
+
+// UpdateTaskHandler defines the route handler function for updating a task
+func UpdateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	taskHandler := models.TaskHandler(db)
+	taskHandler.UpdateTask(w, r, id)
+}
+
+// DeleteTaskHandler defines the route handler function for deleting a task
+func DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	taskHandler := models.TaskHandler(db)
+	taskHandler.DeleteTask(w, r, id)
 }
