12eb8bbd37a603bb3d80e449cd2ac201b71bef4a
christianotieno <christian.otieno@saltpay.co>
2023-07-04 21:43:59 +0100

    feat: associate tasks creation with user

--- server/src/entities/task_entity.go
+++ server/src/entities/task_entity.go
@@ -1,7 +1,10 @@
 package entities
 
 type Task struct {
-	ID      int64  `json:"id"`
-	Summary string `json:"summary"`
-	Date    string `json:"date"`
+	ID         int64  `json:"id"`
+	Summary    string `json:"summary"`
+	Date       string `json:"date"`
+	UserID     int64  `json:"user_id"`
+	Manager    *User  `json:"manager,omitempty"`
+	Technician *User  `json:"technician,omitempty"`
 }
