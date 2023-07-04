8e1917bbe657f5624eddebfc8a212c7adc8d76f4
christianotieno <christian.otieno@saltpay.co>
2023-07-04 01:19:19 +0100

    feat: update route handler to factor in new operations

--- server/src/handlers/route_handler.go
+++ server/src/handlers/route_handler.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strings"
 )
 
 // RouteHandler handles all the routes
@@ -17,9 +18,17 @@ func RouteHandler() {
 	// Define the routes
 	router.HandleFunc("/tasks", CreateTaskHandler).Methods(http.MethodPost)
 	router.HandleFunc("/tasks", GetAllTasksHandler).Methods(http.MethodGet)
+	router.HandleFunc("/tasks/{id}", GetTaskHandler).Methods(http.MethodGet)
+	router.HandleFunc("/tasks/{id}", UpdateTaskHandler).Methods(http.MethodPatch)
+	router.HandleFunc("/tasks/{id}", DeleteTaskHandler).Methods(http.MethodDelete)
 	router.HandleFunc("/", homeHandler)
 
-	log.Println("Server listening on http://localhost:8080")
+	// Redirect URLs with a trailing slash to the non-slash version
+	router.PathPrefix("/tasks/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, strings.TrimSuffix(r.URL.Path, "/"), http.StatusMovedPermanently)
+	})
+
 	// Start the server
+	log.Println("Server listening on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
