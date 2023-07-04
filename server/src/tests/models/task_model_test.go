4102299d0e5fe3d13814344020d0688a986513d7
christianotieno <christian.otieno@saltpay.co>
2023-07-04 01:17:33 +0100

    test: add tests for specific crud operation on tasks model

--- server/src/tests/models/task_model_test.go
+++ server/src/tests/models/task_model_test.go
@@ -1,87 +1,133 @@
-package models
+package models_test
 
 import (
 	"bytes"
 	"database/sql"
 	"encoding/json"
-	"fmt"
-	"net/http"
-	"net/http/httptest"
-	"testing"
-
+	"github.com/christianotieno/tasks-traker-app/server/src/config"
 	"github.com/christianotieno/tasks-traker-app/server/src/entities"
 	"github.com/christianotieno/tasks-traker-app/server/src/models"
 	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
-func TestCreateTask(t *testing.T) {
-	// Create a mock database connection
-	db, _ := createMockDB()
+func TestTaskModel(t *testing.T) {
+	db, err := config.TestDbConnect()
+	if err != nil {
+		t.Fatal(err)
+	}
 	defer func(db *sql.DB) {
 		err := db.Close()
 		if err != nil {
-
+			t.Fatal(err)
 		}
 	}(db)
 
-	// Create a request with a task payload
-	task := entities.Task{Summary: "Test Task", Date: "2023-07-01"}
-	payload, _ := json.Marshal(task)
-	req, _ := http.NewRequest("POST", "/tasks", bytes.NewBuffer(payload))
+	taskModel := models.TaskModel{Db: db}
 
-	// Create a response recorder to record the response
-	rr := httptest.NewRecorder()
+	t.Run("CreateTask", func(t *testing.T) {
+		reqBody := []byte(`{"summary": "Test Summary", "date": "2023-01-01"}`)
+		req, err := http.NewRequest(http.MethodPost, "/tasks", bytes.NewReader(reqBody))
+		assert.NoError(t, err)
 
-	// Initialize the task model handler with the mock DB
-	taskHandler := models.TaskHandler(db)
+		w := httptest.NewRecorder()
 
-	// Call the CreateTask handler function
-	taskHandler.CreateTask(rr, req)
+		taskModel.CreateTask(w, req)
 
-	// Check the response status code
-	assert.Equal(t, http.StatusCreated, rr.Code)
-}
+		assert.Equal(t, http.StatusCreated, w.Code)
 
-func TestGetAllTasks(t *testing.T) {
-	// Create a mock database connection
-	db, _ := createMockDB()
-	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
+		var createdTask entities.Task
+		err = json.Unmarshal(w.Body.Bytes(), &createdTask)
+		assert.NoError(t, err)
 
+		assert.NotNil(t, createdTask.ID)
+		assert.Equal(t, "Test Summary", createdTask.Summary)
+		assert.Equal(t, "2023-01-01 00:00:00", createdTask.Date)
+	})
+
+	t.Run("GetAllTasks", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "/tasks", nil)
+		assert.NoError(t, err)
+
+		w := httptest.NewRecorder()
+
+		taskModel.GetAllTasks(w, req)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		var tasks []entities.Task
+		err = json.Unmarshal(w.Body.Bytes(), &tasks)
+		assert.NoError(t, err)
+
+		assert.NotEmpty(t, tasks)
+	})
+
+	t.Run("GetTask", func(t *testing.T) {
+		_, err := db.Exec("DELETE FROM tasks WHERE id = ?", 1)
+		assert.NoError(t, err)
+
+		_, err = db.Exec("INSERT INTO tasks (id, summary, date) VALUES (?, ?, ?)", 1, "Test Summary", "2023-01-01")
+		assert.NoError(t, err)
+
+		req, err := http.NewRequest(http.MethodGet, "/tasks/1", nil)
+		assert.NoError(t, err)
+
+		w := httptest.NewRecorder()
+
+		taskModel.GetTask(w, req, "1")
+
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		var task entities.Task
+		err = json.Unmarshal(w.Body.Bytes(), &task)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "Test Summary", task.Summary)
+	})
+
+	t.Run("UpdateTask", func(t *testing.T) {
+		_, err := db.Exec("DELETE FROM tasks WHERE id = ?", 1)
+		assert.NoError(t, err)
+
+		_, err = db.Exec("INSERT INTO tasks (id, summary, date) VALUES (?, ?, ?)", 1, "Test Summary", "2023-01-01")
+		assert.NoError(t, err)
+
+		reqBody := []byte(`{"summary": "Updated Summary", "date": "2023-02-02"}`)
+		req, err := http.NewRequest(http.MethodPatch, "/tasks/1", bytes.NewReader(reqBody))
+		assert.NoError(t, err)
+
+		w := httptest.NewRecorder()
+
+		taskModel.UpdateTask(w, req, "1")
+
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		var summary string
+		var date string
+		err = db.QueryRow("SELECT summary, date FROM tasks WHERE id = ?", 1).Scan(&summary, &date)
+		assert.NoError(t, err)
+		assert.Equal(t, "Updated Summary", summary)
+		assert.Equal(t, "2023-02-02 00:00:00", date)
+	})
+
+	t.Run("DeleteTask", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodDelete, "/tasks/1", nil)
+		assert.NoError(t, err)
+
+		w := httptest.NewRecorder()
+
+		taskModel.DeleteTask(w, req, "1")
+
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		var response struct {
+			Message string `json:"message"`
 		}
-	}(db)
+		err = json.Unmarshal(w.Body.Bytes(), &response)
+		assert.NoError(t, err)
 
-	// Create a request
-	req, _ := http.NewRequest("GET", "/tasks", nil)
-
-	// Create a response recorder to record the response
-	rr := httptest.NewRecorder()
-
-	// Initialize the task model handler with the mock DB
-	taskHandler := models.TaskHandler(db)
-
-	// Call the GetAllTasks handler function
-	taskHandler.GetAllTasks(rr, req)
-
-	// Check the response status code
-	assert.Equal(t, http.StatusOK, rr.Code)
-
-	// Check the response body
-	var tasks []entities.Task
-	err := json.Unmarshal(rr.Body.Bytes(), &tasks)
-	if err != nil {
-		return
-	}
-	assert.Len(t, tasks, 2) // Assuming you have 2 tasks in the mock DB
-}
-
-func createMockDB() (*sql.DB, error) {
-	// Open an in-memory SQLite database connection
-	db, err := sql.Open("sqlite3", ":memory:")
-	if err != nil {
-		return nil, fmt.Errorf("failed to open in-memory database: %v", err)
-	}
-
-	return db, nil
+		assert.Equal(t, "Task deleted successfully", response.Message)
+	})
 }
